6a845713f2dd9773e46ea0ad1688ff91b2643736
Ekatereana <ekatereanagricaenko@gmail.com>
2021-01-29 17:14:21 +0200

    add new command

--- background.js
+++ background.js
@@ -9,14 +9,15 @@
 console.log('hello');
 
 chrome.commands.onCommand.addListener(function(command) {
-    if(command === 'test_feature') {
-      console.log('call feature 1');
-      chrome.tabs.query({url : 'https://www.youtube.com/*'}, function(tab){
-        console.log('in query');
-        console.log(0, tab[0]);
-       chrome.tabs.executeScript(tab[0].id, {file: './js/youtubePlayerScript.js'}, function(result){
-         console.log(result);
-       });
-      })
+  chrome.tabs.query({url : 'https://www.youtube.com/*'}, function(tab){
+    if(command === 'current') {
+      chrome.tabs.executeScript(tab[0].id, {code: 'document.getElementsByClassName("ytp-play-button")[0].click();'});
     }
+    
+    if (command === 'next'){
+      chrome.tabs.executeScript(tab[0].id, {code: 'document.getElementsByClassName("ytp-next-button")[0].click();'});
+    }
+    
+  });
+  
   });
